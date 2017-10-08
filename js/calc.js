64d0267ab9c1c6c2462f90351b5d1558c3dac489
Дмитрий Жиляев <Dmitriy@zhilyaev.net>
2017-10-09 02:44:41 +0300

    Починил типы в SessionStorage эх, как мало я еще шарю в гите

--- js/calc.js
+++ js/calc.js
@@ -1,23 +1,10 @@
 "use strict";
-// defaults structure for goal
-const defaults = {
-    cy: '₽',
-    name: 'Цель',
-    dateStart: new Date().yyyymmdd("-"),
-    // (today + 1 year).parse("YYYY-mm-dd")
-    dateFinish: new Date(new Date().setFullYear(new Date().getFullYear() + 1)).yyyymmdd("-"),
-    diffMonths: 12,
-    dream: "", // totalMoney
+/****************************
 
-    /* Percent */
-    broker: 0,
-    brokerPercent: 70,
-    pillow: 0,
-    pillowPercent: 20,
-    reserve: 0,
-    reservePercent: 10
 
-};
+ <GLOBAL FUNCTIONS>
+
+ ****************************/
 /* Str[] to goal[] */
 Object.getPrototypeOf(localStorage).asArrayOfObj = function () {
     let res = [];
@@ -31,48 +18,6 @@ Object.getPrototypeOf(localStorage).asArrayOfObj = function () {
     }
     return res
 };
-/* Calc table data */
-Object.getPrototypeOf(localStorage).calcTable = function () {
-    const goals = this.asArrayOfObj();
-    let table = [];
-
-    // Начало первой цели
-    const beginnerDate = new Date(goals[0].dateStart);
-    // Конец последней цели
-    const finisherDate = new Date(goals[goals.length-1].dateFinish);
-    // Длина всего срока
-    const totalMoths = Date.diff(beginnerDate,finisherDate)[1];
-    //console.log('Весь срок: '+totalMoths+' months');
-    // Переключатель целей
-    let index = 0;
-    // сука, это пенка!
-    let foam = 0;
-/*
-    for(let i=0;i<totalMoths+1;i++){
-        // Setup const
-        //* @Vonvee, don`t change structure of row
-        // * don`t append or delete property
-        const row = {
-            date : new Date(new Date().setMonth(beginnerDate.getMonth()+i)),// Можно прибавлять к beginnerDate++
-            totalMoney: 0,
-            broker: 0,
-            pillow: 0,
-            reserve:0,
-            checked: goals[index].dream<this.totalMoney // Экспереминатльным путем было доказано что это не работает
-        };
-
-        // Цель достигнута идем к следующей
-        if(row.checked){
-            // Собираем пенку
-
-            index++;
-        }
-
-        table.push(row)
-    }*/
-    return table
-};
-
 /* Remove like ListArray */
 Object.getPrototypeOf(localStorage).safeRemove = function (index) {
     for (let i = index; i < this.length; i++)
@@ -80,127 +25,14 @@ Object.getPrototypeOf(localStorage).safeRemove = function (index) {
     //
     delete this[this.length - 1]
 };
-// Primary start
-if (localStorage.length === 0){
-    localStorage[0] = JSON.stringify(defaults);
-    // That works and okay
-    sessionStorage["inCom"] = "";
-    sessionStorage["expense"] = "";
-    sessionStorage["rightNow"] = "";
-    sessionStorage["pillowRightNow"]="";
-    sessionStorage["reserveRightNow"]="";
-}
-// Fix deep event for delTab
-let canDel = false;
-const app = new Vue({
-    el: "#app",
-    data: {
-        current: JSON.parse(localStorage[0]),
-        // Really shit is Magic Link
-        goals: localStorage,
-        // current index
-        i: 0,
 
-        /* rightNow */
-        rightNow: sessionStorage["rightNow"],
-        pillowRightNow: sessionStorage["pillowRightNow"],
-        reserveRightNow: sessionStorage["reserveRightNow"],
-
-        inCom: sessionStorage["inCom"],
-        expense: sessionStorage["expense"],
-        gain: 0
-    },
-    computed: {
-        /* Should you make a new property in $data? */
-        resultPercent: function () {
-            const periodsInYear = 12;
-            let result = rate(periodsInYear,  (-1) * this.current.broker, (-1)* this.rightNow, this.current.dream,0.01);
-            console.log("rate = "+result);
-
-            if (result < 0) return "Цель выполнится накоплением без вкладов";
-            else if (result > 50) return  "Выполнение цели недостижимо в данные сроки";
-            else if (isNaN(result) || this.gain<=0) return 0;
-            else return result.toFixed(4)*this.current.diffMonths
-        }
-    },
-    watch: {
-        // react saving in localStorage
-        current: {
-            handler: function () {
-                this.goals[this.i] = JSON.stringify(this.current)
-            },
-            deep: true
-        },
-        // Fix Percent
-        'current.brokerPercent': function(){
-            this.current.brokerPercent = fixPercent(this.current.brokerPercent,this.current.reservePercent,this.current.pillowPercent)
-        },
-        'current.pillowPercent': function(){
-            this.current.pillowPercent = fixPercent(this.current.pillowPercent,this.current.brokerPercent,this.current.reservePercent)
-        },
-        'current.reservePercent': function() {
-            this.current.reservePercent = fixPercent(this.current.reservePercent,this.current.brokerPercent,this.current.pillowPercent)
-        },
-        // crutches
-        inCom: function () {
-            sessionStorage["inCom"] = this.inCom;
-        },
-        expense: function () {
-            sessionStorage["expense"] = this.expense;
-        },
-        rightNow: function () {
-            sessionStorage["rightNow"] = this.rightNow;
-        },
-        pillowRightNow: function () {
-            sessionStorage["pillowRightNow"] = this.pillowRightNow;
-        },
-        reserveRightNow: function () {
-            sessionStorage["reserveRightNow"] = this.reserveRightNow;
-        }
-    },
-    methods: {
-        newTab: function () {
-            this.current = JSON.parse(JSON.stringify(defaults));
-            this.goals[this.goals.length] = JSON.stringify(this.current);
-            this.i = this.goals.length - 1
-        },
-        delTab: function () {
-            // Fix deep events
-            canDel = true
-        },
-        setActive: function (key) {
-            // has click delTab early
-            if (canDel) {
-                canDel = false;
-                // delTab:
-                this.goals.safeRemove(key);
-                // offset
-                key = 0
-            }
-            // SetActive :
-            this.current = JSON.parse(this.goals[key]);
-            this.i = key
-
-        }
-    }
-});
 function fixPercent(a,b,c) {
-    if(a==="") a=0;
-
+    if(a==="") a=0; // ParseInt(a)
     let sum = a+b+c;
     if(sum>100) a = 100 - (b + c);
-    console.log(a);
-    return a;
+    console.log("FixPercent : "+a);
+    return a
 }
-// Change place by prior
-$("#prior").change(function () {
-    // app.goals === localStorage
-    let t = app.goals[this.value];
-    app.goals[this.value] = app.goals[app.i];
-    app.goals[app.i] = t;
-
-    app.i = this.value
-});
 // СТАВКА
 function rate(paymentsPerYear, paymentAmount, presentValue, futureValue, dueEndOrBeginning, interest) {
     interest = (interest === undefined) ? 0.01 : interest;
@@ -210,7 +42,7 @@ function rate(paymentsPerYear, paymentAmount, presentValue, futureValue, dueEndO
     const FINANCIAL_MAX_ITERATIONS = 128;//Bet accuracy with 128
     const FINANCIAL_PRECISION = 0.0000001;//1.0e-8
 
-    let y, y0, y1, x0, x1 = 0, f = 0, i = 0;
+    let y, y0, y1, x0, x1, f = 0, i;
     let rate = interest;
     if (Math.abs(rate) < FINANCIAL_PRECISION)
     {
@@ -248,5 +80,178 @@ function rate(paymentsPerYear, paymentAmount, presentValue, futureValue, dueEndO
         y1 = y;
         ++i;
     }
-    return rate;
+    return rate
 }
+/****************************
+
+
+ GLOBAL VARIABLES
+
+ ****************************/
+// defaults structure for goal
+const defaults = {
+    cy: '₽',
+    name: 'Цель',
+    dateStart: new Date().yyyymmdd("-"),
+    // (today + 1 year).parse("YYYY-mm-dd")
+    dateFinish: new Date(new Date().setFullYear(new Date().getFullYear() + 1)).yyyymmdd("-"),
+    diffMonths: 12,
+    dream: "", // totalMoney
+
+    /* Percent */
+    broker: 0,  // calculated attr
+    brokerPercent: 70,
+    pillow: 0,  // calculated attr
+    pillowPercent: 20,
+    reserve: 0,  // calculated attr
+    reservePercent: 10
+};
+// Primary start
+const primaryStart = localStorage.length === 0;
+if (primaryStart) localStorage[0] = JSON.stringify(defaults);
+
+
+// Fix deep event for app=>delTab()
+let canDel = false;
+// Global Application
+const app = new Vue({
+    el: "#app",
+    data: {
+        current: JSON.parse(localStorage[0]),
+        // Really shit is Magic Link
+        goals: localStorage,
+        // current index
+        i: 0,
+
+        rightNow: primaryStart ? "" : parseInt(sessionStorage.rightNow),
+        pillowRightNow:  primaryStart ? "" : parseInt(sessionStorage.pillowRightNow),
+        reserveRightNow:  primaryStart ? "" : parseInt(sessionStorage.reserveRightNow),
+
+        inCom:  primaryStart ? "" :parseInt(sessionStorage.inCom),
+        expense:  primaryStart ? "" : parseInt(sessionStorage.expense),
+        // calculated attr
+        gain: 0
+    },
+    computed: {
+        /* Should you make a new property in $data? */
+        resultPercent: function () {
+            let result = rate(this.current.diffMonths,  (-1) * this.current.broker, (-1)* this.rightNow, this.current.dream,0);
+            result = result.toFixed(4)*100;
+            console.log("rate = "+result);
+
+            if (result < 0) return "Цель выполнится накоплением без вкладов";
+            else if (result > 50) return  "Выполнение цели недостижимо в данные сроки";
+            else if (isNaN(result) || this.gain<=0) return 0;
+            else return result
+        },
+        calcTable: function (){
+            const goals = this.goals.asArrayOfObj();
+            let table = [];
+
+            // Начало первой цели
+            const beginnerDate = new Date(goals[0].dateStart);
+            // Конец последней цели
+            const finisherDate = new Date(goals[goals.length-1].dateFinish);
+            // Длина всего срока инвестировния
+            const totalMoths = Date.diff(beginnerDate,finisherDate)[1];
+
+            // Бегунок по ценям
+            // goals.length*goals[i].diffMonths = totalMonths
+            for(let i=0;i<goals.length;i++){
+                //Бегунок по месяцам
+                for(let j=0;j<goals[i].diffMonths;j++){
+                    // Бля ну добавьте возможность уже явное объявление типов
+
+                    // Setup const
+                    //* @Vonvee, don`t change structure of row
+                    // * don`t append or delete property
+                    beginnerDate.setMonth(beginnerDate.getMonth()+1);
+                    console.log(this.pillowRightNow+this.reserveRightNow);
+                    const row = {
+                        date : beginnerDate.ddmmyyyy("."),// beginnerDate++
+                        totalMoney: this.rightNow+this.pillowRightNow+this.reserveRightNow+(goals[i].broker+goals[i].pillow+goals[i].reserve)*j,
+                        broker: this.rightNow+goals[i].broker*j,
+                        pillow: this.pillowRightNow+goals[i].pillow*j,
+                        reserve: this.reserveRightNow+goals[i].reserve*j,
+                        checked: function () {
+                            //console.log((goals[i].broker+goals[i].pillow+goals[i].reserve)*j);
+                            const flag = this.totalMoney>goals[i].dream;
+                            if(flag) console.log("Выполненна цель №"+i);
+                            return flag
+                        }
+                    };
+
+                    table.push(row)
+                }
+            }
+            return table
+        }
+    },
+    watch: {
+        // react saving in localStorage
+        current: {
+            handler: function () {
+                this.goals[this.i] = JSON.stringify(this.current)
+            },
+            deep: true
+        },
+        // Fix Percent
+        'current.brokerPercent': function(){
+            this.current.brokerPercent = fixPercent(this.current.brokerPercent,this.current.reservePercent,this.current.pillowPercent)
+        },
+        'current.pillowPercent': function(){
+            this.current.pillowPercent = fixPercent(this.current.pillowPercent,this.current.brokerPercent,this.current.reservePercent)
+        },
+        'current.reservePercent': function() {
+            this.current.reservePercent = fixPercent(this.current.reservePercent,this.current.brokerPercent,this.current.pillowPercent)
+        },
+        // 1, 2, 3, 4, 8 - Костыли писать не бросим!
+        inCom: function () {
+            sessionStorage.inCom = this.inCom
+        },
+        expense: function () {
+            sessionStorage.expense = this.expense
+        },
+        rightNow: function () {
+            sessionStorage.rightNow = this.rightNow
+        },
+        pillowRightNow: function () {
+            sessionStorage.pillowRightNow = this.pillowRightNow
+        },
+        reserveRightNow: function () {
+            sessionStorage.reserveRightNow = this.reserveRightNow
+        }
+    },
+    methods: {
+        newTab: function () {
+            this.current = JSON.parse(JSON.stringify(defaults));
+            this.goals[this.goals.length] = JSON.stringify(this.current);
+            this.i = this.goals.length - 1
+        },
+        delTab: function () {
+            // Fix deep events
+            canDel = true
+        },
+        setActive: function (key) {
+            // has click delTab early
+            if (canDel) {
+                canDel = false;
+                // delTab:
+                this.goals.safeRemove(key);
+                // offset
+                key = 0
+            }
+            // SetActive :
+            this.current = JSON.parse(this.goals[key]);
+            this.i = key
+        }
+    }
+});
+// Change place by prior
+$("#prior").change(function () {
+    // app.goals === localStorage
+    let t = app.goals[this.value];
+    app.goals[this.value] = app.goals[app.i];
+    app.goals[app.i] = t;
+    app.i = this.value
+});
